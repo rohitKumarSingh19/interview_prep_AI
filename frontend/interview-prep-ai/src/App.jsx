178ef4ae85c9eac0bdc1f8211ef0877f513ee93d
rohitsingh <rohit.k.chauhan1997@gmail.com>
2025-07-27 14:01:18 +0530

    LandingPage page completed

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {Toaster} from 'react-hot-toast';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Home/Dashboard';
+import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
 const App=()=>{
   return(
-    <div className="text-5xl text-orange-600">App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* default Route */}
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep/>}/>
+        </Routes>
+      </Router>
+      <Toaster
+      toastOptions={{
+        className:"",
+        style:{
+          fontSize:"13px",
+        }
+      }}
+      />
+    </div>
   )
 }
 export default App;
