9ac604815391d9be190defc8daa4a0efba1495d3
rohitsingh <rohit.k.chauhan1997@gmail.com>
2025-07-31 14:44:09 +0530

    Frontend LoginPage,SignUp Page is completed

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -4,6 +4,9 @@ import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import {LuSparkles} from 'react-icons/lu';
+import Modal from "../components/Modal";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
 const LandingPage = () => {
   const navigate = useNavigate();
   const [openAuthModal, setOpenAuthModel] = useState(false);
@@ -57,7 +60,66 @@ const LandingPage = () => {
                 className="w-[80vw] rounded-lg"/>
             </section>
         </div>
+        <div className="w-full min-h-full bg-[#FFCEF] mt-10">
+          <div className="container mx-auto px-4 pt-10 pb-20">
+            <section className="mt-5">
+              <h2 className="text-2xl font-medium text-center mb-12">
+                Features That Make You Shine
+              </h2>
+              <div className="flex flex-col items-center gap-8">
+                {/* First 3 Cards */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+                  {APP_FEATURES.slice(0,3).map((feature)=>(
+                    <div
+                    key={feature.id}
+                    className="bg-[#FFFEF8] p-6 rounded-xl shadows-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+                {/* Remaining 2 cards */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                  {APP_FEATURES.slice(3).map((feature)=>{
+                    <div
+                    key={feature.id}
+                    className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                      </div>
+                  })}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
+          Made with ❤️... Happy Coding
+        </div>
       </div>
+      <Modal 
+      isOpen={openAuthModal}
+      onClose={()=>{
+        setOpenAuthModel(false);
+        setCurrentPage("login")
+      }}
+      hideHeader
+      >
+        <div>
+        {currentPage=="login" && (
+          <Login setCurrentPage={setCurrentPage}/>
+        )}
+        {currentPage=="signup" &&(
+          <SignUp setCurrentPage={setCurrentPage}/>
+        )}
+        </div>
+      </Modal>
       </>
   );
 };
